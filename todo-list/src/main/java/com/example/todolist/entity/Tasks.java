0b0f0ead7e8efed8282cf083a31817ff7eb2cfdd
MateuszWitkowiak <boskiraptor2@gmail.com>
2025-11-13 18:21:56 +0100

    Entities implemented, starter SecurityConfig

---
+++ todo-list/src/main/java/com/example/todolist/entity/Tasks.java
@@ -0,0 +1,40 @@
+package com.example.todolist.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+public class Tasks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private Status status;
+
+    @Column(name = "dueDate")
+    private String dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categories_id", nullable = false)
+    private Categories category;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
